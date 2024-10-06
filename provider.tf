253239cbf24c793099848776660dc5045afbf69b
SaiPisey2 <130427660+SaiPisey2@users.noreply.github.com>
2024-10-06 22:21:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,3 +3,18 @@ provider "aws" {
   access_key = ""
   secret_key = ""
 }
+
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.11.0"
+    }
+  }
+}
+
+
+provider "kubernetes" {
+  config_path    = "/root/.kube/config"
+}
